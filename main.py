f94a2587f9448f9d5a60b944d395531c8ad17873
Sean Yap <syap0317@gmail.com>
2021-06-06 22:07:45 -0400

    add feature for bot to reply when it detects sad words

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-import discord
-import os
-import requests
-import json
+import discord  # api wrapper for the discord api 
+import os  # to work with environment variables
+import requests  # make a http request to api
+import json  # to process data returned from api
+import random # for bot to choose message randomly
 
 # create an instance of the client using methods from the discord.py library
 # GENERAL STEP = (create a new object) using library func
 client = discord.Client()
+
 my_secret = os.environ['TOKEN']  # grab the token from environment variable
 
+# words for bot to look out
+sad_words = ["sad", "depressed", "unhappy", "anxious", "miserable", "worse", "depressing", "bad", "pissed", "unexceptable"]
+
+# encouraging words for bot to reply when detects sad words
+# its called initial because user will be able to add more encouragements to the bot in discord 
+initial_encouragements = [
+  "Cheer up!", 
+  "Hang in there.",
+  "You got this!"
+]
 
 # helper function 
 def get_quote():
@@ -56,32 +68,46 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
+  msg = message.content  # because we will use message.content many times so we create a variable for that value
+
+  if msg.startswith('$hello'):
     # check to see if the message starts with a specific string (our command)
     # if true, the bot will response 'Hello!'
     await message.channel.send('Hello!')
 
-  if message.content.startswith('$inspire'):
+  if msg.startswith('$inspire'):
     await message.channel.send(get_quote())
 
-''' feature #1 '''
-# anyone should be able to add stuff for the bot to use 
-# the stuff that the user gives to the bot will be stored in a DATABASE, so those stuff will save even if you stop and start your bot!!!!!
-
-''' feature #2 '''
-# working with API 
-# 1. import python modules: requests, json
-# 2. add a get quote function
-# 3. the bot will then call the function 
-
-# request module: allows our code to make a http request to get data from an api
-# json module: api returns a json, which this module will allow us to work with the json data returned by an api 
-
-
+  # go through every `word` in `sad_words` and check if any of the current `word` is in the `msg`
+  if any(word in msg for word in sad_words):
+    # found a word in the sad_words list in the message
+    await message.channel.send(random.choice(initial_encouragements))
 
 # now we need to run the bot
 # within the run method, we need to put our bot's token (password)
 client.run(my_secret)
+
+''' feature #1 
+1. anyone should be able to add stuff for the bot to use 
+2. the stuff that the user gives to the bot will be stored in a DATABASE, so those stuff will save even if you stop and start your bot!!!!!
+'''
+''' feature #2 
+working with API 
+1. import python modules: requests, json
+2. add a get quote function
+3. the bot will then call the function 
+
+request module: allows our code to make a http request to get data from an api
+json module: api returns a json, which this module will allow us to work with the json data returned by an api 
+'''
+''' feature #3
+bot will recognize sad words that user typed and then reply with encouragement words
+1. create a python list of sad words for the bot to look for
+2. create a python list of encouragement words to reply 
+3. (user will be able to add more encouragement words to the database)
+4. when we receive a message, check the message to see if it contains sad words
+
+'''
 
 # by default, every project on repl.it is public so everyone will be able to see secrets like password
 # so we need to hide these secrets using environment variable
